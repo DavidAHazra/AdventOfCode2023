feef71c2eb52b37e05be0d4dcd53c42c647b62bf
DavidAHazra <david.ogden493@gmail.com>
2023-12-17 17:59:14 +0000

    add day 13

---
+++ M-day-13/python/part-2.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def lines_above_mirror(pattern):
+    for i in range(pattern.shape[0] - 1):
+        above_lines = pattern[:i + 1, :]
+        below_lines = pattern[i + 1:, :]
+
+        max_len = min(above_lines.shape[0], below_lines.shape[0])
+        differences = (above_lines[::-1][:max_len] != below_lines[:max_len]).sum()
+
+        if differences == 1:
+            return i + 1
+
+
+if __name__ == '__main__':
+    # Idea: use binary array for each row, use XOR to find differences
+    note_summary_sum = 0
+    with open("../input.txt", "r") as input_file:
+        for notes in input_file.read().split("\n\n"):
+            notes = np.array([list(line) for line in notes.splitlines()])
+
+            above_mirror = lines_above_mirror(notes)
+
+            if above_mirror is not None:
+                note_summary_sum += 100 * above_mirror
+
+            else:
+                note_summary_sum += lines_above_mirror(notes.T)
+
+    print(f"The sum of the note summaries is: {note_summary_sum}")
+
